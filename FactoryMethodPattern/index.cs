e19e6f1b5c08b2e212b6be5db7b09bc01fff189a
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2022-03-16 20:49:32 -0400

    Comments wrt abstract factory vs factory method

--- FactoryMethodPattern/index.cs
+++ FactoryMethodPattern/index.cs
@@ -1,1 +1,12 @@
 // This example is coming from the book Head First Design Pattern.
+
+
+// Summary:
+
+// Only have one variable which is pizza
+// Whereas for abstract factory pattern, they have more than one
+// variables which are IDough, ISauce, ICheese
+
+// When more variables, it is called abstract factory pattern
+// Whereas one variable, it is called factory method pattern
+public abstract AbstractPizza CreatePizza(string type);
