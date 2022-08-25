373bcb126cc5d21647e2ae3646159399cb697e10
TrevorBai <50292459+TrevorBai@users.noreply.github.com>
2022-08-25 01:31:47 -0400

    Add flyweight pattern sample files

---
+++ FlyweightPattern/Tree.cs
@@ -0,0 +1,24 @@
+﻿namespace FlyweightPattern
+{
+    public class Tree
+    {
+        //private readonly double _xCoord;
+        //private readonly double _yCoord;
+        //private readonly double _age;
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Age { get; set; }
+
+        public void Display(double x, double y, double age)
+        {
+            // Use X - Y coords
+            // & complex age
+            // related calcs
+
+            X = x;
+            Y = y;
+            Age = age;
+        }
+    }
+}
---
+++ FlyweightPattern/TreeManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace FlyweightPattern
+{
+    // An optimization pattern
+    public class TreeManager
+    {
+        // 2D array
+        private readonly List<List<Tree>> _treeList;
+        private double _x;
+        private double _y;
+        private double _age;
+
+        public void DisplayTrees()
+        {
+            foreach (var treeRow in _treeList)
+            {
+                // Get array row
+                foreach (var tree in treeRow)
+                {
+                    tree.Display(tree.X, tree.Y, tree.Age);
+                }
+            }
+        }
+
+        public void SetXCoord(double xCoord)
+        {
+            _x = xCoord;
+        }
+
+        public void SetYCoord(double yCoord)
+        {
+            _y = yCoord;
+        }
+
+        public void SetAge(double age)
+        {
+            _age = age;
+        }
+    }
+}
